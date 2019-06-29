a3b81e9ecdbb31923b81a194c99db3855d45c4dc
Wasin Thonkaew <haxpor@gmail.com>
2019-06-30 06:23:01 +0800

    integrate mysqrt to depend on whether log and exp functions available

--- MathFunctions/src/MathFunctions.cpp
+++ MathFunctions/src/MathFunctions.cpp
@@ -1,15 +1,26 @@
 #include "MathFunctions.h"
+#include "TutorialConfig.h"
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+#include <math.h>
+#endif
+
+#if !defined(HAVE_LOG) || !defined(HAVE_EXP)
 // use approach in https://stackoverflow.com/a/29019938/571227
 #define MINDIFF 2.25e-308
+#endif
 
 double mysqrt(double value)
 {
+  if (value <= 0)
+    return 0;
+
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  return exp(log(value)*0.5);
+#else
   double root = value/3;
   double last;
   double diff = 1;
-  if (value <= 0)
-    return 0;
 
   do {
     last = root;
@@ -17,4 +28,5 @@ double mysqrt(double value)
     diff = root - last;
   } while (diff > MINDIFF || diff < -MINDIFF);
   return root;
+#endif
 }
